56473570304b993983d4219a36654321ff805d12
kaiserfaravi <shahidullahkaiserfaravi@gmail.com>
2021-10-30 22:50:27 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+console.log(process.env);
+const firebaseConfig = {
+    apiKey: "AIzaSyDlBP4XlInIO8e6007dyIllRlfNEnbnz5Q",
+    authDomain: "ema-john-af306.firebaseapp.com",
+    projectId: "ema-john-af306",
+    storageBucket: "ema-john-af306.appspot.com",
+    messagingSenderId: "377549205724",
+    appId: "1:377549205724:web:ac312c65aadc883a1a8419"
+};
+
+export default firebaseConfig;
